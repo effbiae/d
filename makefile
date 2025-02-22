c1295e9756d47a848a448fd5bc2b142fb92f5062
effbiae <effbiae@gmail.com>
2025-02-22 12:02:15 +1100

    add tests to p.py

--- makefile
+++ makefile
@@ -1,14 +1,12 @@
 C=clang-13
-c:a.o t.o c.c makefile
-	$C -O -oc t.o c.c
+c:a.o makefile
 a.o:k.edu/a.c makefile
 	$C -Ofast -oa.o -c $< -nostdlib -s -w -march=icelake-client -funsigned-char -fno-unwind-tables -mavx512f -mavx512dq -mavx512vbmi
-	cd k.edu;for x in _ a;do ln -s $$x.h ../$$x.h; done
-t.o:parserclub/t.c makefile
-	$C -O -Dmain=tmain -c ot.o $<&&ln parserclub/t.h t.h
+	cd k.edu;for x in _ a;do ln -sf $$x.h ../$$x.h; done
 t.k:
 	echo "!3">t.k
 t.check:
 	echo "0 1 2">t.check
-test:c t.k t.check
+test:t.k t.check
+	python p.py
 	./c t.k>t.out&&diff t.out t.check
--- p.py
+++ p.py
@@ -1,40 +1,52 @@
 m=[b"av;",b"['/\\",b":+-*%!&|<>=~,^#_$?@.",b";)]\n "]
-c=[b';'[0]]+list(b' '*256)
+c=list(b';'+b' '*255)
+def d(x):print(rs(x,chr))
 def f(x,s):
  for a in s:c[a]=x
-[f(m[0][i],m[i+1])for i in range(3)]
-def ix(x,c):
- for i in range(len(x)):
-  if c==x[i]:return i
- return -1
+for i in range(3):f(m[0][i],m[i+1])
 def n():
  global i,s
  if s[i]:i+=1;return s[i-1]
  return 0;
 def q():global i,s;return b';'[0]==c[s[i]]
 def E(x):
- while 1+ix(b";[(",n()):
+ while n() in b";[(":
   if not type(x) is type([]):x=[x]
   x=x+[e(t())]
  return x
 def t():
  global s,i
- if q():return tn(0)
- x=n() if b'('[0]-s[i] else x if 3>len(x:=E(s[i])) else x[1]
+ if q():return []
+ x=n() if b'('[0]-s[i] else x[1] if 3>len(x:=E(s[i])) else x
  while b'a'[0]==c[s[i]]:
   x=E(x) if b'['[0]==s[i]else[n(),x]
  return x
 def v():global s,i;return b';'[0]<c[s[i-1]]
 def e(x):
  if q():return x
- v_=v()
- return [f,x,e(t())]if [f:=t(),v()][1]>v_ else [x,e(f)]
+ v_=v();f=t();return [f,x,e(t())]if v()>v_ else [x,e(f)]
 def parse(x):global s,i;s=b'('+bytes(x,'utf-8')+b'\0';i=0;return t()
-def test(o):
- ["x;y","(x;y)","f[x;y]","x+y","x+*y","1+3*x","(+x)%y","(+/x)%#x","x+m[*i]/y"]
 def rs(x,f):
  if type(x) is type([]):return [rs(a,f) for a in x]
  return f(x)
+def test():
+    import sys
+    if 0: #generate test cases
+        print('[')
+        for x in ["x;y","(x;y)","f[x;y]","x+y","x+*y","1+3*x","(+x)%y","(+/x)%#x","x+m[*i]/y"]:
+            print("[%12s,%s ],"%(repr(x),rs(parse(x),chr)))
+        print(']')
+    for x,y in [
+        [       'x;y',['(', 'x', 'y'] ],
+        [     '(x;y)',['(', 'x', 'y'] ],
+        [    'f[x;y]',['f', 'x', 'y'] ],
+        [       'x+y',['+', 'x', 'y'] ],
+        [      'x+*y',['+', 'x', ['*', 'y']] ],
+        [     '1+3*x',['+', '1', ['*', '3', 'x']] ],
+        [    '(+x)%y',['%', ['+', 'x'], 'y'] ],
+        [  '(+/x)%#x',['%', [['/', '+'], 'x'], ['#', 'x']] ],
+        [ 'x+m[*i]/y',['+', 'x', [['/', ['m', ['*', 'i']]], 'y']] ],
+        ]:
+        if (r:=rs(parse(x),chr))!=y:print('!',x,r,'!=',y);sys.exit(1)
 if __name__=='__main__':
- import sys
- print([rs(parse(x.strip()),chr) for x in sys.stdin.readlines()])
+ test()
