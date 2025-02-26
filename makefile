ec77fa07abcb9ca80fc5141a796c8e281131e2b6
effbiae <effbiae@gmail.com>
2025-02-26 13:56:39 +1100

    handle :monad

--- makefile
+++ makefile
@@ -1,24 +1,22 @@
 C=clang-13
 F=-g -w -funsigned-char -fno-unwind-tables -mavx512f -mavx512dq -mavx512vbmi
 K=-Ofast
-u:_.h u.c a.o z.o makefile
-	$C -ou u.c a.o z.o $F
+g:g.c p.o a.o z.o
+	$C -og $^ $F
 a.o:k.edu/a.c makefile
 	$C $K -c $< $F
 z.o:k.edu/z.c makefile
 	$C $K -Dmain=zmain -D_start=_zstart -c $< $F
 p.o:p.c makefile
 	$C -c $< $F
-g:g.c p.o a.o z.o
-	$C -og $^ $F
 k:k.edu/?.[ch] makefile
 	$C $K -ok k.edu/?.c -nostdlib $F
 _.h:
-	for x in _ a;do ln -sf k.edu/$$x.h $$x.h; done
+	ln -sf k.edu/_.h _.h
 t.check:
 	echo "0 1 2">t.check
 test:t.check
 	python3 p.py
 	echo '!3'|./c>t.out&&diff t.out t.check
 clean:
-	rm *.o u _.h a.h
+	rm -f g *.o _.h
--- p.py
+++ p.py
@@ -1,42 +1,49 @@
 m=[b"av;",b"['/\\",b":+-*%!&|<>=~,^#_$?@.",b";)]\n "]
 c=list(b';'+b' '*255)
-def d(x):print(rs(x,chr))
 def f(x,s):
  for a in s:c[a]=x
 for i in range(3):f(m[0][i],m[i+1])
+def s0(i):return s[i] if type(s[i])==type(b' '[0]) else s[i][0]
+def cs(i):return c[s0(i)]
 def n():
- global i,s
+ global i
  if s[i]:i+=1;return s[i-1]
  return 0;
-def q():global i,s;return b';'[0]==c[s[i]]
+def q():return b';'[0]==cs(i)
 def E(x):
  while n() in b";[(":
   if not type(x) is type([]):x=[x]
   x=x+[e(t())]
  return x
 def t():
- global s,i
  if q():return []
- x=n() if b'('[0]-s[i] else x[1] if 3>len(x:=E(s[i])) else x
- while b'a'[0]==c[s[i]]:
-  x=E(x) if b'['[0]==s[i]else[n(),x]
+ x=n() if b'('[0]-s0(i) else x[1] if 3>len(x:=E(s[i])) else x
+ while b'a'[0]==cs(i):
+  x=E(x) if b'['[0]==s0(i)else[n(),x]
  return x
-def v():global s,i;return b';'[0]<c[s[i-1]]
+def v():return b';'[0]<cs(i-1)
 def e(x):
  if q():return x
  v_=v();f=t();return [f,x,e(t())]if v()>v_ else [x,e(f)]
+def lex(x):
+ r=[];i=0
+ while i<len(x):
+  if not x[i]or x[i+1]-b':'[0]:r.append(x[i])
+  else:r.append(x[i:2+i]);i+=1
+  i+=1
+ return r
 def parse(x):
- global s,i;s=b'('+bytes(x,'utf-8')+b'\0';i=0;r=t();r=rs(r,chr)
- if type(r)is type([])and x[0]!='('and r[0]=='(':r[0]='['
+ global s,i;s=lex(b'('+bytes(x,'utf-8')+b'\0');i=0;r=t();r=rs(r,chr)
+ if type(r)is type([])and len(r)and x[0]!='('and r[0]=='(':r[0]='['
  return r
 def rs(x,f):
  if type(x) is type([]):return [rs(a,f) for a in x]
- return f(x)
+ return f(x) if type(x)is type(1) else ''.join([f(a)for a in x])
 def test():
     import sys
-    if 1: #generate test cases
+    if 0: #generate test cases
         print('[')
-        for x in ["x;y","(x;y)","f[x;y]","x+y","x+*y","1+3*x","(+x)%y","(+/x)%#x","x+m[*i]/y","(-*=)/","5(+\|)\\'"]:
+        for x in ["x;y","(x;y)","f[x;y]","x+y","x+*y","1+3*x","(+x)%y","(+/x)%#x","x+m[*i]/y","3*:/2","5(+\\|:)\\x"]:
             print("[%12s,%s ],"%(repr(x),parse(x)))
         print(']')
     for x,y in [
@@ -49,8 +56,8 @@ def test():
         [    '(+x)%y',['%', ['+', 'x'], 'y'] ],
         [  '(+/x)%#x',['%', [['/', '+'], 'x'], ['#', 'x']] ],
         [ 'x+m[*i]/y',['+', 'x', [['/', ['m', ['*', 'i']]], 'y']] ],
-        [    '(-*=)/',['/', ['-', ['*', '=']]] ],
-        [ '5(+\|)\\x',[['\\', [['\\', '+'], '|']], '5', 'x'] ],
+        [     '3*:/2',[['/', '*:'], '3', '2'] ],
+        ['5(+\\|:)\\x',[['\\', [['\\', '+'], '|:']], '5', 'x'] ],
         ]:
         if (r:=parse(x))!=y:print('!',x,r,'!=',y);sys.exit(1)
 if __name__=='__main__':
