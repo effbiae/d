4b47047b53dc71ab9a99c64acb2c19ad1004cd23
effbiae <effbiae@gmail.com>
2025-03-11 14:33:26 +1100

    tidy

--- p.h
+++ p.h
@@ -1,4 +1,4 @@
 #include"_.h"
 U k(i2,U,U),k_(i2,U),tn(i2,i2);
-typedef U(*uf)(U),(*ug)(U,U);
-U scan(ug gg,U x);U over(ug gg,U x);U each(uf ff,U x);U niters(uf,i2,U);
+typedef U(*uf)(U),(*uF)(U,U);
+U scan(uF,U);U over(uF,U);U each(uf,U);U niters(uf,i2,U);
