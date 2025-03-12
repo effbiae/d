b4db6f9c7992b43775e0329ad9871c53569e6aef
effbiae <effbiae@gmail.com>
2025-03-12 15:44:36 +1100

    tidy

--- p.c
+++ p.c
@@ -9,7 +9,7 @@ Oa(U x){tx==2?O("%d",ix):O("%f",ex);}O0(U x){O("(");i(nx,prin(at(i,rx));$(i+1<$,
 OA(U x){P(nx==1,O(",");prin(at(0,rx)))P(!tx||!nx,O0(x));i2*a=k_(2,x);i(nx,tx==2?O("%d",a[i]):O("%f",((e2*)a)[i]);$(i+1<$,O(" ")));r0(x);}
 prin(U x){P(ax,Oa(x))OA(x);}print(U x){prin(x);O("\n");}
 #define xat(b) i2 b##at(i2 i,Ux){U a=at(i,r1(x));i2 r=x(a,b##x);r0(a);return r;}
-xat(t)xat(a)f(sqz,P(ax||tx||!nx,x);i2 t=tat(0,x);i(nx,P(!aat(i,x)||t!=tat(i,x),x));U r=tn(t,nx);i2*p=k_(2,r);U*q=k_(2,x);i(nx,p[i]=*(i2*)(q+i));r0(x);r;)
+xat(t)xat(a)f(sqz,P(ax||tx||!nx,x)i2 t=tat(0,x);i(nx,P(!aat(i,x)||t!=tat(i,x),x))U r=tn(t,nx);i2*p=k_(2,r);U*q=k_(2,x);i(nx,p[i]=*(i2*)(q+i));r0(x);r;)
 U scan(uF w,U x){$(ax,x=en(x));P(!nx,ti(-1));U r=tn(0,nx);U*p=k_(2,r);*p=at(0,rx);i(nx-1,p[i+1]=w(r1(p[i]),at(i+1,rx)));r0(x);return sqz(r);}
 f(last,P(ax,x)P(!nx,el)at(nx-1,x))
 U over(uF w,U x){return last(scan(w,x));}
