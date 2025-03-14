805d1580289fcf95a4e3fdead9b0f4a4f990d749
effbiae <effbiae@gmail.com>
2025-03-14 19:43:50 +1100

    O now prints generic lists on newlines and c handles projections

--- c
+++ c
@@ -5,13 +5,17 @@ def v(x):return P.find(x)-1
 def tr(x):
  global tri;name='tr'+str(tri);tri+=1
  if type(x) is str:
-  p=v(x[0]);assert(-1<p)
+  p=v(x[0]);assert(-2<p)
   if len(x)==1:trains[name]='F(%s,k(%s,a,x))'%(name,p)
   else:assert(x[1]==':');trains[name]='f(%s,k(%s,0,x))'%(name,p)
  else:
   if x[0]=='o':trains[name]='f(%s,%s(%s(x)))'%(name,tr(x[1]),tr(x[2]))
-  else:#is a derived verb
-   assert(x[0]=='\\');trains[name]='f(%s,scan(%s,x))'%(name,tr(x[1]))
+  else:
+   if type(x[0])==str:#a projection
+    p=v(x[0]);assert(-2<p and len(x)==3 and x[2]==' ')
+    trains[name]='f(%s,k(%s,%s,x))'%(name,p,e(x[1]))
+   else:#is a derived verb
+    assert(x[0]=='\\');trains[name]='f(%s,scan(%s,x))'%(name,tr(x[1]))
  return name
 def e(x):
  if train(x):print(x);return 0
@@ -21,7 +25,7 @@ def e(x):
   if a[0]=='\\':assert(len(x)==3);return f'niters({tr(a[1])},{x[1]},{e(x[2])})'
  if a=='[':return "(%s)"%",".join([e(x) for x in x])
  p=v(a)
- if -1<p:
+ if 0<p:
   if len(x)==2:return "k(%d, 0,%s)"%(p,e(x[1]))
   if len(x)==3:return "k(%d,%s,%s)"%(p,e(x[1]),e(x[2]))
  if a=='::':return la[x[1]]
--- p.c
+++ p.c
@@ -5,9 +5,9 @@
 #define rx r1(x)
 #define el tn(0,0)
 g(at,k(12,x,ti(i)))F(cat,k(17,a,x))f(en,k(17,0,x))static i2 omx=50;void dot(i2 n){$(n>omx,O(".."));}
-Oa(U x){tx==2?O("%d",ix):O("%f",ex);}O0(U x){O("(");i(l(nx,omx),prin(at(i,rx));$(i+1<$,O(";")));dot(nx);O(")");r0(x);}
-OA(U x){P(nx==1,O(",");prin(at(0,rx)))P(!tx||!nx,O0(x));i2*a=k_(2,x);i(l(nx,100),tx==2?O("%d",a[i]):O("%f",((e2*)a)[i]);$(i+1<$,O(" ")));dot(nx);r0(x);}
-prin(U x){P(ax,Oa(x))OA(x);}print(U x){prin(x);O("\n");}
+Oa(U x){tx==2?O("%d",ix):O("%f",ex);}O0(i2 t,U x){O("(");i(l(nx,omx),prin(0,at(i,rx));$(i+1<$,O(t?"\n ":";")));dot(nx);O(")");r0(x);}
+OA(i2 t,U x){P(nx==1,O(",");prin(0,at(0,rx)))P(!tx||!nx,O0(t,x));i2*a=k_(2,x);i(l(nx,100),tx==2?O("%d",a[i]):O("%f",((e2*)a)[i]);$(i+1<$,O(" ")));dot(nx);r0(x);}
+prin(i2 t,U x){P(ax,Oa(x))OA(t,x);}print(U x){prin(1,x);O("\n");}
 #define xat(b) i2 b##at(i2 i,Ux){U a=at(i,r1(x));i2 r=x(a,b##x);r0(a);return r;}
 xat(t)xat(a)f(sqz,P(ax||tx||!nx,x)i2 t=tat(0,x);i(nx,P(!aat(i,x)||t!=tat(i,x),x))U r=tn(t,nx);i2*p=k_(2,r);U*q=k_(2,x);i(nx,p[i]=*(i2*)(q+i));r0(x);r;)
 U scan(uF w,U x){$(ax,x=en(x));P(!nx,ti(-1));U r=tn(0,nx);U*p=k_(2,r);*p=at(0,rx);i(nx-1,p[i+1]=w(r1(p[i]),at(i+1,rx)));r0(x);return sqz(r);}
