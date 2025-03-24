50ed68bcf4d8a2c61f891496235f3f4f4db2a2d5
effbiae <effbiae@gmail.com>
2025-03-24 12:23:05 +1100

    flipped _r and r_ convention

--- c.c
+++ c.c
@@ -1,11 +1,11 @@
 #include"c.h"
 #define O printf
 static i2 omx=50;void dot(i2 n){$(n>omx,O(".."));}
-Oa(U x){tx==2?O("%d",ix):O("%f",ex);}O0(i2 t,U x){O("(");i(l(nx,omx),prin(0,at(rx,i));$(i+1<$,O(t?"\n ":";")));dot(nx);O(")");r_(x);}
-OA(i2 t,U x){P(nx==1,O(",");prin(0,at(rx,0)))P(!tx||!nx,O0(t,x));i2*a=k_(2,x);i(l(nx,100),tx==2?O("%d",a[i]):O("%f",((e2*)a)[i]);$(i+1<$,O(" ")));dot(nx);r_(x);}
+Oa(U x){tx==2?O("%d",ix):O("%f",ex);}O0(i2 t,U x){O("(");i(l(nx,omx),prin(0,at(rx,i));$(i+1<$,O(t?"\n ":";")));dot(nx);O(")");_r(x);}
+OA(i2 t,U x){P(nx==1,O(",");prin(0,at(rx,0)))P(!tx||!nx,O0(t,x));i2*a=k_(2,x);i(l(nx,100),tx==2?O("%d",a[i]):O("%f",((e2*)a)[i]);$(i+1<$,O(" ")));dot(nx);_r(x);}
 prin(i2 t,U x){P(ax,Oa(x))OA(t,x);}print(U x){prin(1,x);O("\n");}
-U scan(uF w,U x){$(ax,x=en(x));P(!nx,x);U r=tn(0,nx);U*p=k_(2,r);*p=at(rx,0);i(nx-1,p[i+1]=w(_r(p[i]),at(rx,i+1)));r_(x);return sqz(r);}
+U scan(uF w,U x){$(ax,x=en(x));P(!nx,x);U r=tn(0,nx);U*p=k_(2,r);*p=at(rx,0);i(nx-1,p[i+1]=w(r_(p[i]),at(rx,i+1)));_r(x);return sqz(r);}
 f(last,P(ax||!nx,x)at(x,nx-1))
 U over(uF w,U x){return last(scan(w,x));}
-U each(uf w,U x){$(ax,x=en(x));U r=tn(0,nx);U*p=k_(2,r);i(nx,p[i]=w(at(rx,i)));r_(x);return sqz(r);}
-U niters(uf w,i2 n,U x){U r=tn(0,n+1);U*p=k_(2,r);*p=_r(x);i(n,p[i+1]=w(_r(p[i])));return sqz(r);}
+U each(uf w,U x){$(ax,x=en(x));U r=tn(0,nx);U*p=k_(2,r);i(nx,p[i]=w(at(rx,i)));_r(x);return sqz(r);}
+U niters(uf w,i2 n,U x){U r=tn(0,n+1);U*p=k_(2,r);*p=r_(x);i(n,p[i+1]=w(r_(p[i])));return sqz(r);}
--- c.h
+++ c.h
@@ -1,8 +1,8 @@
 #include"_.h"
-#define _r(x) k_(1,x)
-#define r_(x) k_(0,x)
-#define rx _r(x)
-#define ra _r(a)
+#define _r(x) k_(0,x)
+#define r_(x) k_(1,x)
+#define rx r_(x)
+#define ra r_(a)
 #define el tn(0,0)
 #include<stdio.h>
 #define O printf
@@ -11,11 +11,11 @@
 #define Urp(n) Ur=tn(0,n);U*p=k_(2,r)
 U k(i2,U,U),k_(i2,U),tn(i2,i2); _U(xx,*(i2*)0)
 _U(at,k(12,x,ti(i)),Ux,ii)
-f(en,Ua=k(17,0,rx);P(!ax&&ta,r_(a);Urp(1);p[0]=x;r)r_(x);a) //bug in en when !ax..
+f(en,Ua=k(17,0,rx);P(!ax&&ta,_r(a);Urp(1);p[0]=x;r)_r(x);a) //bug in en when !ax..
 F(cat,/*dbg("cat%d%d%d%d$\n",aa,ta,ax,tx);$(!aa||ta!=2||ax||tx,0){print(ra);print(rx);};*/
-		P(ta&&!tx,$(aa,a=en(a));Urp(na+nx);i(na,p[i]=at(ra,i));i(nx,p[i+na]=at(rx,i));r_(a);r_(x);/*print(_r(r));dbg("tac");*/r)k(17,a,x))
-#define xat(b) static i2 b##at(Ux,i2 i){U a=at(_r(x),i);i2 r=x(a,b##x);r_(a);return r;}
-xat(t)xat(a)f(sqz,P(ax||tx||!nx,x)i2 t=tat(x,0);i(nx,P(!aat(x,i)||t!=tat(x,i),x))U r=tn(t,nx);i2*p=k_(2,r);U*q=k_(2,x);i(nx,p[i]=*(i2*)(q+i));r_(x);r;)
+		P(ta&&!tx,$(aa,a=en(a));Urp(na+nx);i(na,p[i]=at(ra,i));i(nx,p[i+na]=at(rx,i));_r(a);_r(x);/*print(r_(r));dbg("tac");*/r)k(17,a,x))
+#define xat(b) static i2 b##at(Ux,i2 i){U a=at(r_(x),i);i2 r=x(a,b##x);_r(a);return r;}
+xat(t)xat(a)f(sqz,P(ax||tx||!nx,x)i2 t=tat(x,0);i(nx,P(!aat(x,i)||t!=tat(x,i),x))U r=tn(t,nx);i2*p=k_(2,r);U*q=k_(2,x);i(nx,p[i]=*(i2*)(q+i));_r(x);r;)
 _U(_2,Urp(2);p[0]=x;p[1]=y;sqz(r),Ux,U y)_U(_3,Urp(3);p[0]=x;p[1]=y;p[2]=z;sqz(r),Ux,U y,U z)
 typedef U(*uf)(U),(*uF)(U,U);
 U scan(uF,U);U over(uF,U);U each(uf,U);U niters(uf,i2,U);
