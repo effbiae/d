7d62327785d107427e88c7d17e90a438bd9ab4f3
effbiae <effbiae@gmail.com>
2025-03-24 12:16:54 +1100

    tidy

--- c.h
+++ c.h
@@ -11,9 +11,9 @@
 #define Urp(n) Ur=tn(0,n);U*p=k_(2,r)
 U k(i2,U,U),k_(i2,U),tn(i2,i2); _U(xx,*(i2*)0)
 _U(at,k(12,x,ti(i)),Ux,ii)
-f(en,Ua=k(17,0,rx);P(!ax&&ta!=0,r_(a);Urp(1);p[0]=x;r)a) //bug in en when !ax..
+f(en,Ua=k(17,0,rx);P(!ax&&ta,r_(a);Urp(1);p[0]=x;r)r_(x);a) //bug in en when !ax..
 F(cat,/*dbg("cat%d%d%d%d$\n",aa,ta,ax,tx);$(!aa||ta!=2||ax||tx,0){print(ra);print(rx);};*/
-		P(ta&&tx==0,$(aa,a=en(a));Urp(na+nx);i(na,p[i]=at(ra,i));i(nx,p[i+na]=at(rx,i));r_(a);r_(x);/*print(_r(r));dbg("tac");*/r)k(17,a,x))
+		P(ta&&!tx,$(aa,a=en(a));Urp(na+nx);i(na,p[i]=at(ra,i));i(nx,p[i+na]=at(rx,i));r_(a);r_(x);/*print(_r(r));dbg("tac");*/r)k(17,a,x))
 #define xat(b) static i2 b##at(Ux,i2 i){U a=at(_r(x),i);i2 r=x(a,b##x);r_(a);return r;}
 xat(t)xat(a)f(sqz,P(ax||tx||!nx,x)i2 t=tat(x,0);i(nx,P(!aat(x,i)||t!=tat(x,i),x))U r=tn(t,nx);i2*p=k_(2,r);U*q=k_(2,x);i(nx,p[i]=*(i2*)(q+i));r_(x);r;)
 _U(_2,Urp(2);p[0]=x;p[1]=y;sqz(r),Ux,U y)_U(_3,Urp(3);p[0]=x;p[1]=y;p[2]=z;sqz(r),Ux,U y,U z)
