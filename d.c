94569700a20d063fcc7ce3fb8bb209f923473da0
effbiae <effbiae@gmail.com>
2025-03-25 13:36:33 +1100

    d.c parse works, pc works

--- d.c
+++ d.c
@@ -2,20 +2,17 @@
 #include<string.h>
 #define O printf
 #undef t
-/*str2ints*/_U(p_,in=strlen(s);Ur=tn(2,n);i2*p=sr;i(n,p[i]=*s++);r,ss)_D(i0*,_p,i0*r=malloc(nx+1);i2*p=sx;i(nx,r[i]=p[i]);r[nx]=0;_r(x);r,Ux)
+/*str2ints*/_U(p_,in=strlen(s);Ur=tn(2,n);i2*p=sr;i(n,p[i]=*s++);r,ss)
 /*print*/_U($i,Z0 b[8];int n=sprintf(b,"%d",i);A(n<8);p_(b),ii)
 _Z(prini,$(ax||tx!=2,print(x);xx());i2*p=sx;i(nx,fputc(p[i],f));_r(x),Ux,FILE*f)
-_Z(printi,prini(cat(x,ti('\n')),stdout),Ux)
-_Z(printt,$(ax,prini(cat(x,tn(2,0)),stdout)){O("(");i(nx,printt(at(rx,i)));O(")");_r(x);},Ux)
+_Z(printi,prini(x,stdout);O("\n"),Ux)
+_Z(pu,$(ax,prini(cat(x,tn(2,0)),stdout)){O("(");i(nx,pu(at(rx,i)));O(")");_r(x);},Ux)_Z(printt,pu(x);O("\n"),Ux)
 /*parse*/Z0*m[]={"av;","['/\\",":+-*%&|<>=~.!@?#_^,$LMS...ERZ",";)]\n "},*P,c[128];_D(int,fi,sd=s-1;W(*++d)P(*d==c,d-s);-1,ss,cc)_D(int,v,fi(P,c)-1,cc)
-#undef bi
-#define bi bat(i)
-#define nb x(b,nx)
-ZU b;static i2 i;_U(bat,P(i>=nb,' ')x(at(r_(b),i),ix),ii)
-ZU t(),e(U);_U(n,ti(r(bi,i++)))_D(i2,q,';'==c[bi])f(E,W(-1<fi(";[(",x(n(),ix)))x=cat(x,en(e(t())));x)
-_D(i2,av,';'<c[bat(i-1)])f(e,P(q(),x)i2 v_=av();U f=t();av()>v_?_3(f,x,e(t())):_2(x,e(f)))
-_U(t,P(q(),el)Ux='('!=bi?n():(x=E(ti(bi)),3>nx)?at(x,1):x;W('a'==c[bi])x='['==bi?E(x):_2(n(),x);x)
-_U(parse,b=cat(ti('('),p_(s));i=0;t(),ss)
+static i2 i,*sb,nb;_D(i2,s,P(i>=nb,' ')sb[i],ii)
+ZU t(),e(U);_U(n,ti(r(s(i),i++)))_D(i2,q,';'==c[s(i)])f(E,W(-1<fi(";[(",x(n(),ix)))x=cat(x,en(e(t())));x)
+_D(i2,av,';'<c[s(i-1)])f(e,P(q(),x)i2 v_=av();U f=t();av()>v_?_3(f,x,e(t())):_2(x,e(f)))
+_U(t,P(q(),el)Ux='('!=s(i)?n():(x=E(ti(s(i))),3>nx)?at(x,1):x;W('a'==c[s(i)])x='['==s(i)?E(x):_2(n(),x);x)
+_U(parse,Ux=cat(ti('('),p_(s));sb=sx;nb=nx;i=0;r(t(),_r(x)),ss)
 /*init*/_Z(ini,k_();P=m[2];i(128,c[i]=' ')i(3,sd=m[i+1];W(*d)c[*d++]=m[0][i]))
 /*template*/_U(pc,A(!ax&&tx==0);Ur=tn(2,0);ii=0;h(strlen(s),r=cat(r,s[h]=='%'?at(rx,i++):ti(s[h])));A(nx==i);_r(x);r,ss,Ux)
 /*gen*/Z0*ed[4]={"\\/'","scan","over","each"};f(d,h(3,P(x(at(x,0),ix)==ed[0][h],p_(ed[h+1])));0)
@@ -24,9 +21,8 @@ _f(fgh,Ur=tn(0,3);U*p=sr;i0*e="f\0g\0h";i(3,p[i]=p_(e+i*2));r)
 int main(int n,i0**v){ini();
  U x=en(ti(3));
  print(fgh(0));
- //prini(stdout,pc("f(%,%(%(x)));\n",fgh(0)));
+ printi(pc("f(%,%(%(x)));\n",fgh(0)));
  sd="x[*i]/y";
- print(parse(d));
- printt(parse(d));O("\n");
+ printt(parse(d));
  return 0;
 }
