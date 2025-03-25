dc0e63b826b598a2420b546cb5d28b5d67e44829
effbiae <effbiae@gmail.com>
2025-03-25 16:59:42 +1100

    match(mat) works. parse tests pass

--- d.c
+++ d.c
@@ -5,8 +5,8 @@
 #define Pv(x,z) if(x){_(z);return;}
 /*str2ints*/_U(p_,in=strlen(s);Ur=tn(2,n);i2*p=sr;i(n,p[i]=*s++);r,ss)
 /*print*/_U($i,Z0 b[8];int n=sprintf(b,"%d",i);A(n<8);p_(b),ii)
-_Z(prini,Pv(ax,prini(en(x),f))Pv(tx!=2,print(x);xx())i(nx,fputc(((i2*)sx)[i],f));_r(x),Ux,FILE*f)_Z(printi,prini(x,stdout);O("\n"),Ux)
-f(t2s,P(tx,x)Ur=ti('(');i(nx,r=cat(r,t2s(at(rx,i))))r=cat(r,ti(')'));r(r,_r(x)))
+_Z(p2,Pv(ax,p2(en(x)))Pv(tx!=2,print(x);xx())i(nx,O("%c",((i2*)sx)[i]));_r(x),Ux)_Z(printi,p2(x);O("\n"),Ux)
+f(t2s,A(!tx|tx==2);P(ax,en(x))Ur=ti('(');i(nx,r=cat(r,t2s(at(rx,i))))r=cat(r,ti(')'));_r(x);r)
 /*parse*/Z0*m[]={"av;","['/\\",":+-*%&|<>=~.!@?#_^,$LMS...ERZ",";)]\n "},*P,c[128];_U(fi,sd=s-1;W(*++d)P(*d==ix,ti(d-s));0,ss,Ux)f(v,fi(P,x))
 static i2 i,*sb,nb;_D(i2,s,P(i>=nb,' ')sb[i],ii)
 ZU t(),e(U);_U(n,ti(r(s(i),i++)))_D(i2,q,';'==c[s(i)])f(E,W(fi(";[(",n()))x=cat(x,en(e(t())));x)
@@ -17,12 +17,10 @@ _U(parse,Ux=cat(ti('('),p_(s));sb=sx;nb=nx;i=0;r(t(),_r(x)),ss)
 /*template*/_U(pc,P(ax||tx,pc(s,en(x)))Ur=tn(2,0);ii=0;h(strlen(s),r=cat(r,s[h]=='%'?at(rx,i++):ti(s[h])));A(nx==i);_r(x);r,ss,Ux)
 /*gen*/Z0*ed[4]={"\\/'","scan","over","each"};f(d,h(3,P(x(at(x,0),ix)==ed[0][h],p_(ed[h+1])));0)
 f(eg,P(ax&&ix>='0'&&ix<='9',pc("ti(%)",x));0)
-_Z(Ax,$(!ix,O("err"));,Ux)F(mat,P(ax,ti(ix==ia))P(ta!=tx||na!=nx,ti(0))i(nx,P(!mat(at(rx,i),at(ra,i)),ti(0)))ti(1))
-Z0*pts[][2]={{"0","0"},{"1+2","+12"},{"+/x","(/+)x"},{0}};_Z(pt,i2 i=0;W(*pts[i])(Ax(mat(t2s(parse(pts[i][0])),p_(pts[i][1]))),i++))
+F(mat,in=1;$(ta-tx||(ax?ix-ia:na-nx),n=0);ii=0;W(n&&!ax&&i<nx){U m=mat(at(ra,i),at(rx,i));$(!x(m,ix),n=0);i++;}_r(a);_r(x);ti(n))
+_Z(Aeq,$(!x(mat(ra,rx),ix),p2(a);O("!~");p2(x);O("\n");A(0));_r(a);_r(x),Ua,Ux)
+Z0*pts[][2]={{"0","0"},{"1+2","(+12)"},{"+/x","((/+)x)"},{0}};_Z(pt,i2 i=0;W(*pts[i])(Aeq(t2s(parse(pts[i][0])),p_(pts[i][1])),i++))
 int main(int n,i0**v){ini();
- Ux=t2s(parse("0"));
- printi(x);
- printi(t2s(parse("1+2")));
- printi(eg(parse("0")));
+ pt();
  return 0;
 }
