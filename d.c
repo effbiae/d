1e4aa770eee1cec305790f21b705b423148cc6e4
effbiae <effbiae@gmail.com>
2025-03-25 15:01:23 +1100

    t2s

--- d.c
+++ d.c
@@ -2,14 +2,14 @@
 #include<string.h>
 #define O printf
 #undef t
+#define Pv(x,z) if(x){_(z);return;}
 /*str2ints*/_U(p_,in=strlen(s);Ur=tn(2,n);i2*p=sr;i(n,p[i]=*s++);r,ss)
 /*print*/_U($i,Z0 b[8];int n=sprintf(b,"%d",i);A(n<8);p_(b),ii)
-_Z(prini,$(ax||tx!=2,print(x);xx());i2*p=sx;i(nx,fputc(p[i],f));_r(x),Ux,FILE*f)
-_Z(printi,prini(x,stdout);O("\n"),Ux)
-_Z(pu,$(ax,prini(cat(x,tn(2,0)),stdout)){O("(");i(nx,pu(at(rx,i)));O(")");_r(x);},Ux)_Z(printt,pu(x);O("\n"),Ux)
-/*parse*/Z0*m[]={"av;","['/\\",":+-*%&|<>=~.!@?#_^,$LMS...ERZ",";)]\n "},*P,c[128];_D(int,fi,sd=s-1;W(*++d)P(*d==c,d-s);-1,ss,cc)_D(int,v,fi(P,c)-1,cc)
+_Z(prini,Pv(ax,prini(en(x),f))Pv(tx!=2,print(x);xx())i(nx,fputc(((i2*)sx)[i],f));_r(x),Ux,FILE*f)_Z(printi,prini(x,stdout);O("\n"),Ux)
+f(t2s,P(tx,x)Ur=ti('(');i(nx,r=cat(r,t2s(at(rx,i))))r=cat(r,ti(')'));r(r,_r(x)))
+/*parse*/Z0*m[]={"av;","['/\\",":+-*%&|<>=~.!@?#_^,$LMS...ERZ",";)]\n "},*P,c[128];_U(fi,sd=s-1;W(*++d)P(*d==ix,ti(d-s));0,ss,Ux)f(v,fi(P,x))
 static i2 i,*sb,nb;_D(i2,s,P(i>=nb,' ')sb[i],ii)
-ZU t(),e(U);_U(n,ti(r(s(i),i++)))_D(i2,q,';'==c[s(i)])f(E,W(-1<fi(";[(",x(n(),ix)))x=cat(x,en(e(t())));x)
+ZU t(),e(U);_U(n,ti(r(s(i),i++)))_D(i2,q,';'==c[s(i)])f(E,W(fi(";[(",n()))x=cat(x,en(e(t())));x)
 _D(i2,av,';'<c[s(i-1)])f(e,P(q(),x)i2 v_=av();U f=t();av()>v_?_3(f,x,e(t())):_2(x,e(f)))
 _U(t,P(q(),el)Ux='('!=s(i)?n():(x=E(ti(s(i))),3>nx)?at(x,1):x;W('a'==c[s(i)])x='['==s(i)?E(x):_2(n(),x);x)
 _U(parse,Ux=cat(ti('('),p_(s));sb=sx;nb=nx;i=0;r(t(),_r(x)),ss)
@@ -17,7 +17,12 @@ _U(parse,Ux=cat(ti('('),p_(s));sb=sx;nb=nx;i=0;r(t(),_r(x)),ss)
 /*template*/_U(pc,P(ax||tx,pc(s,en(x)))Ur=tn(2,0);ii=0;h(strlen(s),r=cat(r,s[h]=='%'?at(rx,i++):ti(s[h])));A(nx==i);_r(x);r,ss,Ux)
 /*gen*/Z0*ed[4]={"\\/'","scan","over","each"};f(d,h(3,P(x(at(x,0),ix)==ed[0][h],p_(ed[h+1])));0)
 f(eg,P(ax&&ix>='0'&&ix<='9',pc("ti(%)",x));0)
+_Z(Ax,$(!ix,O("err"));,Ux)F(mat,P(ax,ti(ix==ia))P(ta!=tx||na!=nx,ti(0))i(nx,P(!mat(at(rx,i),at(ra,i)),ti(0)))ti(1))
+Z0*pts[][2]={{"0","0"},{"1+2","+12"},{"+/x","(/+)x"},{0}};_Z(pt,i2 i=0;W(*pts[i])(Ax(mat(t2s(parse(pts[i][0])),p_(pts[i][1]))),i++))
 int main(int n,i0**v){ini();
+ Ux=t2s(parse("0"));
+ printi(x);
+ printi(t2s(parse("1+2")));
  printi(eg(parse("0")));
  return 0;
 }
