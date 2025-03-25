9d5b7e76e11c340e6c9102221b751607b80ebe16
effbiae <effbiae@gmail.com>
2025-03-25 18:00:10 +1100

    tidy

--- d.c
+++ d.c
@@ -19,8 +19,6 @@ _U(parse,Ux=cat(ti('('),p_(s));sb=sx;nb=nx;i=0;r(t(),_r(x)),ss)
 f(eg,P(ax&&ix>='0'&&ix<='9',pc("ti(%)",x));0)
 F(mat,in=1;$(ta-tx|(ax?ix-ia:na-nx),n=0);ii=0;W(n&&!ax&&i<nx){U m=mat(at(ra,i),at(rx,i));n=x(m,ix);i++;}_r(a);_r(x);ti(n))
 _Z(Aeq,U m=mat(ra,rx);$(!x(m,ix),p2(a);O("!~");p2(x);O("\n");xx());_r(a);_r(x),Ua,Ux)
-Z0*pts[][2]={{"0","0"},{"1+2","(+12)"},{"+/x","((/+)x)"},{"x[*i]'y","(('(x(*i)))y)"},{0}};_Z(pt,i2 i=0;W(*pts[i])(Aeq(t2s(parse(pts[i][0])),p_(pts[i][1])),i++))
-int main(int n,i0**v){ini();
- pt();
- return 0;
-}
+Z0*pts[][2]={{"",""},{"0","0"},{"1+2","(+12)"},{"+/x","((/+)x)"},{"x[*i]'y","(('(x(*i)))y)"},{0}};
+_Z(pt,i2 i=0;W(*pts[i])(Aeq(t2s(parse(pts[i][0])),p_(pts[i][1])),i++))
+int main(int n,i0**v){ini();pt();return 0;}
