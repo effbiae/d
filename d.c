347e9418116b7d873413c0b01e5aa4029731e96f
effbiae <effbiae@gmail.com>
2025-03-26 15:02:18 +1100

    parse, gen and dylink

--- d.c
+++ d.c
@@ -4,7 +4,7 @@ U strlen(const char*);int system(char*);
 #define O printf
 #undef t
 #define Pv(x,z) if(x){_(z);return;}
-/*str2ints*/_U(p_,in=strlen(s);Ur=tn(2,n);i2*p=sr;i(n,p[i]=*s++);r,ss)
+/*str2ints*/_U(p_,in=strlen(s);Ur=tn(2,n);i2*p=sr;i(n,p[i]=*s++);r,ss)_Z(pO,A(tx==2);i2*p=sx;i(nx,fputc(p[i],f));_r(x),FILE*f,Ux);
 /*print*/_U($i,Z0 b[8];int n=sprintf(b,"%d",i);A(n<8);p_(b),ii)
 _Z(p2,Pv(ax,p2(en(x)))Pv(tx!=2,print(x);xx())i(nx,O("%c",((i2*)sx)[i]));_r(x),Ux)_Z(printi,p2(x);O("\n"),Ux)
 f(t2s,A(!tx|tx==2);P(ax,en(x))Ur=ti('(');i(nx,r=cat(r,t2s(at(rx,i))))r=cat(r,ti(')'));_(r))
@@ -22,8 +22,9 @@ F(mat,in=1;$(ta-tx|(ax?ix-ia:na-nx),n=0);ii=0;W(n&&!ax&&i<nx){U m=mat(at(ra,i),a
 F(Aeq,Ur=mat(ra,rx);$(!x(r,ix),p2(pc("~%~%\n",_2(ra,rx)));xx());_r(a);_x(r))
 Z0*pts[][2]={{"","()"},{"0","0"},{"1+2","(+12)"},{"+/x","((/+)x)"},{"x[*i]'y","(('(x(*i)))y)"},{0}};
 _Z(pt,i2 i=0;W(*pts[i])(Aeq(t2s(parse(*pts[i])),p_(pts[i][1])),i++))
-int main(int n,i0**v){ini();pt();
-	FILE*g=fopen("h.c","w");fprintf(g,"#include\"d.h\"\nU d(){return _k(17,0,ti(3));}\n");fclose(g);system("make h.so 2>&1|head");
-	void*l=dlopen("./h.so",RTLD_NOW);$(!l,O("%s\n",dlerror())); void(*ini)()=dlsym(l,"ini");ini(k,tn);U(*d)()=dlsym(l,"d");print(d());
-	ini(&k,&tn);p2(d());
+U dl(U e){
+FILE*g=fopen("h.c","w");pO(g,pc("#include\"d.h\"\nU d(){return %;}\n",eg(e)));fclose(g);system("make h.so 2>&1|head");
+void*l=dlopen("./h.so",RTLD_NOW);$(!l,O("%s\n",dlerror())); void(*ini)()=dlsym(l,"ini");ini(k,tn);U(*d)()=dlsym(l,"d");d();
+ini(&k,&tn);return d();}
+int main(int n,i0**v){ini();pt();print(dl(parse("4")));
 	return 0;}
