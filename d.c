f504c632bb52fda2386126918537ac6ca52800e4
effbiae <effbiae@gmail.com>
2025-03-28 16:59:43 +1100

    rounded out eg adverbs

--- d.c
+++ d.c
@@ -11,8 +11,8 @@ U strlen(const char*);int system(char*);
 /*parse*/Z0*mi[]={"av;","['/\\",Ps,";)]\n "},c[256];_U(fi,sd=s-1;W(*++d)P(*d==ix,ti(d-s));0,ss,Ux)f(v,fi(mi[2],ti((i0)ix)))
 static i2 i,*sb,ns;_D(i2,mo,i<ns-1&&v(sb[i])&&sb[i+1]==':')_D(i2,s,P(i>=ns,' ')sb[i]|mo()<<8,ii)ZU t(),e(U);
 _U(n,ti(r(s(i),i+=1+mo())))_D(i2,q,';'==c[(i0)s(i)])f(E,W(fi(";[(",n()))x=cat(x,en(e(t())));x)
-_D(i2,av,';'<c[(i0)s(i-1)])f(e,P(q(),x)i2 v_=av();U f=t();av()>v_?_3(f,x,e(t())):_2(x,e(f)))
-_U(t,P(q(),el)Ux='('!=(i0)s(i)?n():(x=E(ti(s(i))),3>nx)?at(x,1):x;W('a'==c[(i0)s(i)])x='['==s(i)?E(x):_2(n(),x);x)
+_D(i2,av,';'<c[(i0)s(i-1)])f(e,P(q(),x)i2 v=av();U f=t();av()>v?_3(f,x,e(t())):_2(x,e(f)))
+_U(t,P(q(),el)Ux='('!=s(i)?n():(x=E(ti(s(i))),3>nx)?at(x,1):x;W('a'==c[(i0)s(i)])x='['==s(i)?E(x):_2(n(),x);x)
 _U(parse,Ux=cat(ti('('),p_(s));sb=sx;ns=nx;i=0;_x(t()),ss)
 /*parse test*/Z0*pts[][2]={{"","()"},{"0","0"},{"1+2","(+12)"},{"+/x","((/+)x)"},{"x[*i]'y","(('(x(*i)))y)"},{"!:'x","(('!:)x)"},{0}};
 f(t2s,A(!tx|tx==2);P(ax,en(x))Ur=ti('(');i(nx,r=cat(r,t2s(at(rx,i))))r=cat(r,ti(')'));_x(r))_Z(pt,i2 i=0;W(*pts[i])(Amat(t2s(parse(*pts[i])),p_(pts[i][1])),i++))
@@ -22,9 +22,11 @@ f(train,U p;Ua=fns;Ur=cat(p_("tr"),$i(ti(na)));fms=cat(fms,en(rr));
 		A(ax);
 		P(ax&&(p=v(ix)),U u=_2(rr,$i(p));fns=cat(fns,en(pc(ix>>8?"f(%,k(%,0,x))":"F(%,k(%,a,x))",u)));r);0)
 f(eg,P(ax&&ix>='0'&&ix<='9',pc("ti(%)",x));
-     A(!ax&&nx);U a=at(rx,0);
-     U p=aa?v(ra):0;P(p,_r(a);U t=_2($i(p),eg(at(rx,1)));nx==2?_x(pc("k(%,0,%)",t)):pc("k(%,%,%)",cat(t,en(eg(at(x,2))))))
-     A(na==2);A(ad(at(ra,0)));pc("%(%,%)",_3(ad(at(ra,0)),train(at(a,1)),eg(at(x,1)))))
+     A(!ax&&nx);Ua=at(rx,0);U p=aa?v(ra):0;
+     P(p,_r(a);U t=_2($i(p),eg(at(rx,1)));nx==2?_x(pc("k(%,0,%)",t)):pc("k(%,%,%)",cat(t,en(eg(at(x,2))))))
+     $(na==2,P(nx==2,pc("%(%,%)",_3(ad(at(ra,0)),train(at(a,1)),eg(at(x,1)))))
+	     P(nx==3,fi("\\",at(ra,0))?pc("niters(%,%,%)",_3(train(at(a,1)),at(rx,1),eg(at(x,2)))):(p1(pc("no dyadic %",en(at(a,0)))),_r(x),0))
+     );0)
 _U(fs,Ux=fns;Ua=fms;Ur=p_("");i(nx,r=cat(r,at(rx,i)));r)
 f(dl,//pass a parse tree, get back its evaluation
  //p1(t2s(r_(x)));
