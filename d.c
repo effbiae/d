90393fdaf9cf8e07abb9e374bcc2d6f6e9932c7c
effbiae <effbiae@gmail.com>
2025-03-31 14:07:34 +1100

    squeeze

--- d.c
+++ d.c
@@ -21,21 +21,19 @@ _U(parse,Ux=cat(ti('['),p_(s));sb=sx;ns=nx;i=0;_x(t()),ss)
 f(t2s,A(!tx|tx==2);P(ax,ix>>8?cat(ti((i0)ix),ti(':')):en(x))Ur=ti('(');i(nx,r=cat(r,t2s(at(rx,i))))r=cat(r,ti(')'));_x(r))
 _Z(pt,Ux=mat(ti(1),ti(2));A(!ix);i2 i=0;W(*pts[i])(Amat(t2s(parse(*pts[i])),p_(pts[i][1])),i++))
 /*gen*/Z0*ed[4]={"\\/'","scan","over","each"};f(ad,U r=fi(*ed,x);r?p_(ed[x(r,ix)+1]):0)
-ZU fns;static i2 fni;_Z(fn0,fni=0;fns=tn(2,0))ZU eg(Ux);f(cfn,fns=cat(fns,x))
+ZU fns;static i2 fni;_Z(fn0,fni=0;fns=tn(2,0))ZU eg(Ux);_U(fn,fns=cat(fns,pc(s,x)),ss,Ux)
 f(tra,Ua=ax||nx<1?0:x0;U p=ax?v(x):0;Ur=pc("t%",en($i(ti(fni++))));
-	$(p,U u=_2(rr,$i(p));cfn(pc(ix>>8?"f(%,k(%,0,x))":"F(%,k(%,a,x))",u)))
-	 $(a&&aa,$(fi("'",a),_x(cfn(pc("f(%,%(%(x)))",_3(rr,tra(x1),tra(x2))))))//composition
-		  $(p=ad(a),_x(cfn(pc("f(%,%(%,x))",_3(rr,p,tra(x1))))))//adverb
-	           $(p=v(a),A(nx==3);_x(cfn(pc("f(%,k(%,%,x))",_3(rr,$i(p),eg(x1))))));//projection
+	$(p,U u=_2(rr,$i(p));fn(ix>>8?"f(%,k(%,0,x))":"F(%,k(%,a,x))",u))
+	 $(a&&aa,$(fi("'",a),_x(fn("f(%,%(%(x)))",_3(rr,tra(x1),tra(x2)))))//composition
+		  $(p=ad(a),_x(fn("f(%,%(%,x))",_3(rr,p,tra(x1)))))//adverb
+	           $(p=v(a),A(nx==3);_x(fn("f(%,k(%,%,x))",_3(rr,$i(p),eg(x1)))));//projection
 	  )$(tx==2,_r(r);r=x);
 	 r)
-F(la,a=en(a);cfn(pc("f(%,%)",_2(ra,eg(x))));a)
+F(la,a=en(a);fn("f(%,%)",_2(ra,eg(x)));a)
 /* 1_ */f(one_,P(ax,tn(tx,0))P(!nx,x)Ur=el;ii=0;W(++i<nx)r=cat(r,en(at(rx,i)));_x(r))
 /* ","/ */f(cj,P(ax,en(x))P(nx<2,x)ii=0;Ur=x0;W(++i<nx)r=cat(r,cat(ti(','),at(rx,i)));_x(r))
 f(eg,
-     P(ax&&ix>='0'&&ix<='9',pc("ti(%)",x))P(ax,pc("r_(%)",x))Ua=x0;
-     P(aa&&(i0)ia==':'&&ia>>8,A(nx==3);_x(la(x1,x2)))
-     P(aa&&ia=='[',pc("(%)",cj(each(eg,one_(x)))))
+     P(ax&&ix>='0'&&ix<='9',pc("ti(%)",x))P(ax,pc("r_(%)",x))Ua=x0;P(aa&&(i0)ia==':'&&ia>>8,A(nx==3);_x(la(x1,x2)))P(aa&&ia=='[',pc("(%)",cj(each(eg,one_(x)))))
      $(na==2,P(nx==2,_x(pc("%(%,%)",_3(ad(at(ra,0)),tra(at(a,1)),eg(x1)))))
 	     P(nx==3,_x(fi("\\",at(ra,0))?pc("niters(%,%,%)",_3(tra(at(a,1)),x1,eg(x2))):(p1(pc("no dyadic %",at(a,0))),0)))
      )P(train(ra),U t=_2(tra(a),eg(x1));_x(nx==2?pc("%(%)",t):pc("%(%,%)",cat(t,en(eg(x2))))))
